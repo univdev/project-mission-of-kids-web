7b602a02b58d796562d7be5b52c8568bd3efd931
univdev <chanyoung@starshell.co>
2019-12-18 01:21:55 +0900

    랭크 그래프에 반영

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -13,10 +13,22 @@
   .content {
     flex: 1;
     padding: 40px 60px;
+    overflow: auto;
   }
   .record {
     width: 230px;
     background-color: white;
+  }
+  .chart {
+    margin-top: 40px;
+  }
+  .line-chart-container {
+    width: 100%;
+    max-width: 1000px;
+    height: 300px;
+  }
+  .line-chart {
+    height: 300px;
   }
   .title {
     font-size: 22px;
@@ -47,12 +59,6 @@
     margin-top: 40px;
     font-weight: bold;
   }
-  .chart {
-    margin-top: 40px;
-  }
-  .line-chart {
-    height: 300px;
-  }
 </style>
 
 <template lang="pug">
@@ -70,9 +76,11 @@
             .banner-link VIEW DETAIL
       .chart
         .title Chart
-        app-line-chart.line-chart(
-          :style="{ height: '250px', position: 'relative' }",
-          :chart-data="chartData")
+        .line-chart-container
+          app-line-chart.line-chart(
+            :chart-data="chartData",
+            :width="600",
+            :height="300")
     .record
       .best-card
         .best-title
@@ -83,8 +91,11 @@
 </template>
 
 <script>
+import _ from 'lodash';
 import moment from 'moment';
 import AppLineChart from '~/components/AppLineChart';
+
+let LOAD_INTERVAL = null;
 
 export default {
   name: 'Index',
@@ -93,10 +104,9 @@ export default {
   },
   async asyncData({ $axios }) {
     try {
-      // const since = moment().set(-7, 'day').format('YYYY-MM-DD');
-      // const until = moment().format('YYYY-MM-DD');
-      // const params = { since, until };
-      const params = {};
+      const since = moment().set(-7, 'day').format('YYYY-MM-DD');
+      const until = moment().format('YYYY-MM-DD');
+      const params = { since, until };
       const { data: ranks } = await $axios.get('/ranks', { params });
       return { ranks };
     } catch (e) {
@@ -114,28 +124,81 @@ export default {
     },
     ranksSplitByDate() {
       const result = {};
-      this.ranks.forEach((item) => {
-        const key = moment(item.createdAt).format('YYYY-MM-DD');
-        if (!(key in result)) result[key] = [];
-        result.push(key);
+      this.oneWeek.forEach(date => {
+        const format = date.format('YYYY-MM-DD');
+        result[format] = this.ranks.filter(item => moment(item.createdAt).format('YYYY-MM-DD') === format);
       });
       return result;
     },
+    bestRanksSplitByDate() {
+      const result = [];
+      const ranksSplitByDate = _.cloneDeep(this.ranksSplitByDate);
+      Object.keys(ranksSplitByDate).forEach(date => {
+        const items = ranksSplitByDate[date].sort((a, b) => {
+          const aScore = a.score;
+          const bScore = b.score;
+          if (aScore > bScore) return -1;
+          if (aScore < bScore) return 1;
+          return 0;
+        });
+        const firstItem = items[0];
+        if (firstItem) result.push(firstItem);
+        else result.push(null);
+      });
+      return result;
+    },
+    bestRank() {
+      return this.bestRanksSplitByDate.filter(item => item).sort((a, b) => {
+        const aScore = a.score;
+        const bScore = b.score;
+        if (aScore > bScore) return -1;
+        if (aScore < bScore) return 1;
+        return 0;
+      })[0];
+    },
     chartData() {
+      const data = this.bestRanksSplitByDate.map(item => {
+        if (item) return item.score;
+        return null;
+      });
       return {
         labels: this.oneWeek.map((item) => item.format('YYYY-MM-DD')),
         datasets: [
           {
             label: 'Records',
             backgroundColor: '#F3A800',
-            data: [2, 3, 7, 2, 1, 5, 3],
+            data,
           },
         ],
       };
     },
+    async load() {
+      try {
+        const since = moment().set(-7, 'day').format('YYYY-MM-DD');
+        const until = moment().format('YYYY-MM-DD');
+        const params = { since, until };
+        const { data } = await this.$axios.get('/ranks', { params });
+        return data;
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
   },
   mounted() {
-
+    LOAD_INTERVAL = setInterval(async () => {
+      try {
+        this.ranks = await this.load();
+      } catch (e) {
+        console.error(e);
+        if (!e.response) return;
+        const { message } = e.response.data;
+        this.$message.error(message);
+      }
+    }, 3000);
+  },
+  beforeDestroy() {
+    clearInterval(LOAD_INTERVAL);
   },
 };
 </script>
